b2aa3fadc84bda38a06223d73a83c46dca8e507b
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2023-08-22 00:43:40 +0100

    changed github svg to facebook svg

--- app/signup/accountType.tsx
+++ app/signup/accountType.tsx
@@ -7,7 +7,7 @@ const AccountType = ({ handleBack }: { handleBack: () => void }) => {
           onClick={handleBack}
         >
           {/* <span> */}
-          <svg
+          {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
             fill="currentColor"
@@ -18,8 +18,19 @@ const AccountType = ({ handleBack }: { handleBack: () => void }) => {
               d="M18 10a.75.75 0 01-.75.75H4.66l2.1 1.95a.75.75 0 11-1.02 1.1l-3.5-3.25a.75.75 0 010-1.1l3.5-3.25a.75.75 0 111.02 1.1l-2.1 1.95h12.59A.75.75 0 0118 10z"
               clipRule="evenodd"
             />
+          </svg> */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="w-5 h-5"
+          >
+            <polyline points="15 18 9 12 15 6" />
           </svg>
-          {/* </span> */}
           Go back
         </button>
       </div>
