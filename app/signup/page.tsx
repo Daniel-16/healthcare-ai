b660bf1307a5830fbcc0efbfa0e0c2431f697234
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2023-08-24 23:26:11 +0100

    code cleanup

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,170 +1,151 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
-import AccountType from "./accountType";
 import ShowPassword from "@/components/ShowPassword";
 
 const Signup = () => {
-  const [step, setStep] = useState(0);
-  const handleNext = () => {
-    setStep(step + 1);
-    console.log(step);
-  };
-  const handleBack = () => {
-    setStep(step - 1);
-    console.log(step);
-  };
-  if (step == 0) {
-    return (
-      <>
-        <main className="w-full flex flex-col items-center justify-center px-4">
-          <div className="max-w-sm w-full text-gray-600">
-            <div className="text-center">
-              <h1 className="text-gray-800 text-4xl font-bold">Healthcare</h1>
-              <div className="mt-3 space-y-2">
-                <h3 className="text-gray-800 text-2xl font-bold sm:text-3xl">
-                  Sign up
-                </h3>
-                <p className="">
-                  Already have an account?{" "}
-                  <Link
-                    href="/login"
-                    className="font-medium text-indigo-600 hover:text-indigo-500"
-                  >
-                    Log in
-                  </Link>
-                </p>
+  return (
+    <>
+      <main className="w-full flex flex-col items-center justify-center px-4">
+        <div className="max-w-sm w-full text-gray-600">
+          <div className="text-center">
+            <h1 className="text-gray-800 text-4xl font-bold">Healthcare</h1>
+            <div className="mt-3 space-y-2">
+              <h3 className="text-gray-800 text-2xl font-bold sm:text-3xl">
+                Sign up
+              </h3>
+              <p className="">
+                Already have an account?{" "}
+                <Link
+                  href="/login"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Log in
+                </Link>
+              </p>
+            </div>
+          </div>
+          <form onSubmit={(e) => e.preventDefault()} className="mt-8 space-y-4">
+            <div>
+              <label className="font-medium">Name</label>
+              <input
+                type="text"
+                required
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                placeholder="John Doe"
+              />
+            </div>
+            <div>
+              <label className="font-medium">Email</label>
+              <input
+                type="email"
+                required
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                placeholder="johndoe@mail.com"
+              />
+            </div>
+            <div>
+              <label className="font-medium">Choose account type</label>
+              <div className="mx-auto relative">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 right-2.5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <select
+                  required
+                  className="w-full px-3 py-2 text-gray-500 bg-white border rounded-lg shadow-sm outline-none appearance-none focus:border-indigo-700"
+                >
+                  <option>Patient</option>
+                  <option>Doctor</option>
+                </select>
               </div>
             </div>
-            <form
-              onSubmit={(e) => e.preventDefault()}
-              className="mt-8 space-y-4"
-            >
-              <div>
-                <label className="font-medium">Name</label>
-                <input
-                  type="text"
-                  required
-                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                  placeholder="John Doe"
-                />
-              </div>
-              <div>
-                <label className="font-medium">Email</label>
-                <input
-                  type="email"
-                  required
-                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                  placeholder="johndoe@mail.com"
-                />
-              </div>
-              <div>
-                <label className="font-medium">Choose account type</label>
-                <div className="mx-auto relative">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 right-2.5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                  <select
-                    required
-                    className="w-full px-3 py-2 text-gray-500 bg-white border rounded-lg shadow-sm outline-none appearance-none focus:border-indigo-700"
-                  >
-                    <option>Patient</option>
-                    <option>Doctor</option>
-                  </select>
-                </div>
-              </div>
-              <div>
-                <label className="font-medium">Password</label>
-                {/* <input
+            <div>
+              <label className="font-medium">Password</label>
+              {/* <input
                   type="password"
                   required
                   className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
                   placeholder="Must have at least 6 characters"
                 /> */}
-                <ShowPassword placeholder="Must have at least 6 characters" />
-              </div>
-              <button
-                type="submit"
-                className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
-                onClick={handleNext}
-              >
-                Create account
-              </button>
-            </form>
-            <div className="grid grid-cols-3 gap-x-3 mt-3.5 md:mb-4">
-              {/* Google svg button */}
-              <button className="flex items-center justify-center py-2.5 border rounded-lg hover:bg-gray-50 duration-150 active:bg-gray-100">
-                <svg
-                  className="w-5 h-5"
-                  viewBox="0 0 48 48"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <g clipPath="url(#clip0_17_40)">
-                    <path
-                      d="M47.532 24.5528C47.532 22.9214 47.3997 21.2811 47.1175 19.6761H24.48V28.9181H37.4434C36.9055 31.8988 35.177 34.5356 32.6461 36.2111V42.2078H40.3801C44.9217 38.0278 47.532 31.8547 47.532 24.5528Z"
-                      fill="#4285F4"
-                    />
-                    <path
-                      d="M24.48 48.0016C30.9529 48.0016 36.4116 45.8764 40.3888 42.2078L32.6549 36.2111C30.5031 37.675 27.7252 38.5039 24.4888 38.5039C18.2275 38.5039 12.9187 34.2798 11.0139 28.6006H3.03296V34.7825C7.10718 42.8868 15.4056 48.0016 24.48 48.0016Z"
-                      fill="#34A853"
-                    />
-                    <path
-                      d="M11.0051 28.6006C9.99973 25.6199 9.99973 22.3922 11.0051 19.4115V13.2296H3.03298C-0.371021 20.0112 -0.371021 28.0009 3.03298 34.7825L11.0051 28.6006Z"
-                      fill="#FBBC04"
-                    />
-                    <path
-                      d="M24.48 9.49932C27.9016 9.44641 31.2086 10.7339 33.6866 13.0973L40.5387 6.24523C36.2 2.17101 30.4414 -0.068932 24.48 0.00161733C15.4055 0.00161733 7.10718 5.11644 3.03296 13.2296L11.005 19.4115C12.901 13.7235 18.2187 9.49932 24.48 9.49932Z"
-                      fill="#EA4335"
-                    />
-                  </g>
-                  <defs>
-                    <clipPath id="clip0_17_40">
-                      <rect width="48" height="48" fill="white" />
-                    </clipPath>
-                  </defs>
-                </svg>
-              </button>
-              {/* Twitter svg button */}
-              <button className="flex items-center justify-center py-2.5 border rounded-lg hover:bg-gray-50 duration-150 active:bg-gray-100">
-                <svg
-                  className="w-5 h-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
-                </svg>
-              </button>
-              {/* Facebook svg bFacebook svg buttonutton */}
-              <button className="flex items-center justify-center py-2.5 border rounded-lg hover:bg-gray-50 duration-150 active:bg-gray-100">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="w-5 h-5"
-                  fill="#3b5998"
-                >
-                  <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3 8h-1.35c-.538 0-.65.221-.65.778v1.222h2l-.209 2h-1.791v7h-3v-7h-2v-2h2v-2.308c0-1.769.931-2.692 3.029-2.692h1.971v3z" />
-                </svg>
-              </button>
+              <ShowPassword placeholder="Must have at least 6 characters" />
             </div>
+            <button
+              type="submit"
+              className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
+            >
+              Create account
+            </button>
+          </form>
+          <div className="grid grid-cols-3 gap-x-3 mt-3.5 md:mb-4">
+            {/* Google svg button */}
+            <button className="flex items-center justify-center py-2.5 border rounded-lg hover:bg-gray-50 duration-150 active:bg-gray-100">
+              <svg
+                className="w-5 h-5"
+                viewBox="0 0 48 48"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g clipPath="url(#clip0_17_40)">
+                  <path
+                    d="M47.532 24.5528C47.532 22.9214 47.3997 21.2811 47.1175 19.6761H24.48V28.9181H37.4434C36.9055 31.8988 35.177 34.5356 32.6461 36.2111V42.2078H40.3801C44.9217 38.0278 47.532 31.8547 47.532 24.5528Z"
+                    fill="#4285F4"
+                  />
+                  <path
+                    d="M24.48 48.0016C30.9529 48.0016 36.4116 45.8764 40.3888 42.2078L32.6549 36.2111C30.5031 37.675 27.7252 38.5039 24.4888 38.5039C18.2275 38.5039 12.9187 34.2798 11.0139 28.6006H3.03296V34.7825C7.10718 42.8868 15.4056 48.0016 24.48 48.0016Z"
+                    fill="#34A853"
+                  />
+                  <path
+                    d="M11.0051 28.6006C9.99973 25.6199 9.99973 22.3922 11.0051 19.4115V13.2296H3.03298C-0.371021 20.0112 -0.371021 28.0009 3.03298 34.7825L11.0051 28.6006Z"
+                    fill="#FBBC04"
+                  />
+                  <path
+                    d="M24.48 9.49932C27.9016 9.44641 31.2086 10.7339 33.6866 13.0973L40.5387 6.24523C36.2 2.17101 30.4414 -0.068932 24.48 0.00161733C15.4055 0.00161733 7.10718 5.11644 3.03296 13.2296L11.005 19.4115C12.901 13.7235 18.2187 9.49932 24.48 9.49932Z"
+                    fill="#EA4335"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_17_40">
+                    <rect width="48" height="48" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+            </button>
+            {/* Twitter svg button */}
+            <button className="flex items-center justify-center py-2.5 border rounded-lg hover:bg-gray-50 duration-150 active:bg-gray-100">
+              <svg
+                className="w-5 h-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+              </svg>
+            </button>
+            {/* Facebook svg bFacebook svg buttonutton */}
+            <button className="flex items-center justify-center py-2.5 border rounded-lg hover:bg-gray-50 duration-150 active:bg-gray-100">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="w-5 h-5"
+                fill="#3b5998"
+              >
+                <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3 8h-1.35c-.538 0-.65.221-.65.778v1.222h2l-.209 2h-1.791v7h-3v-7h-2v-2h2v-2.308c0-1.769.931-2.692 3.029-2.692h1.971v3z" />
+              </svg>
+            </button>
           </div>
-        </main>
-      </>
-    );
-  } else {
-    return <AccountType handleBack={handleBack} />;
-  }
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Signup;
