3b026ff8cfaf818e04ab9dc831e6c10e5ccb334b
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2023-08-25 20:45:47 +0100

    link to page

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -7,12 +7,12 @@ const HeroSection = () => {
     <section className="mt-24 mx-auto max-w-screen-xl pb-12 px-4 items-center lg:flex md:px-8">
       <div className="space-y-4 flex-1 sm:text-center lg:text-left">
         <h1 className="text-indigo-700 font-bold text-4xl xl:text-5xl">
-          A platform for finding a Doctor
+          Connecting with Doctors made easier through AI
         </h1>
         <p className="text-gray-600 max-w-xl leading-relaxed sm:mx-auto lg:ml-0">
-          It is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at its layout. The point of
-          using Lorem Ipsum
+          By leveraging AI technology, we are transforming the way patients gets
+          medical attention by connecting patients to doctors without needing to
+          visit a hospital.
         </p>
         <div className="pt-10 items-center justify-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex lg:justify-start">
           <Link
