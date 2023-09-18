4953f1f6bd49507c8209adf92aa3343d2d320cbb
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2023-09-19 00:14:41 +0100

    feat: back button component

---
+++ components/BackButton.tsx
@@ -0,0 +1,22 @@
+const BackButton = () => {
+  return (
+    <div className="p-3">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="cursor-pointer"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M19 12H6M12 5l-7 7 7 7" />
+      </svg>
+    </div>
+  );
+};
+
+export default BackButton;
