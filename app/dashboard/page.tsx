a12d82021cd8e479880aa08ab9a2314a1c226420
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2023-10-13 23:20:23 +0100

    feat: sidebar

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import Sidebar from "@/components/Sidebar";
+
+const DashBoard = () => {
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
+};
+
+export default DashBoard;
---
+++ components/Sidebar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="drawer lg:drawer-open">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col items-center justify-center">
+        {/* Page content here */}
+        <label
+          htmlFor="my-drawer-2"
+          className="btn btn-primary drawer-button lg:hidden"
+        >
+          Open drawer
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-2"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+          {/* Sidebar content here */}
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
