103eaa18d3bc0e7a9ffbabc24723dfe22c61876c
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2023-10-17 08:27:50 +0100

    edited sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -19,7 +19,7 @@ const Sidebar = () => {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+        <ul className="menu p-4 w-80 min-h-full bg-neutral-200 text-base-content">
           {/* Sidebar content here */}
           <li>
             <a>Sidebar Item 1</a>
