f4326660cab9dca02b6b392d95d7a765c419b9e6
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2023-12-07 09:59:19 +0100

    feat: search for doctors based on specialization

--- server/controllers/getDoctor.js
+++ server/controllers/getDoctor.js
@@ -1,4 +1,5 @@
 import UserModel from "../models/user.js";
+import DoctorModel from "../models/doctor.js";
 
 export const getAllDoctors = async (req, res) => {
   try {
@@ -11,6 +12,28 @@ export const getAllDoctors = async (req, res) => {
     });
   } catch (error) {
     res.status(404).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export const searchDoctor = async (req, res) => {
+  const { specialization } = req.body;
+  try {
+    const doctor = await DoctorModel.find({ specialization });
+    if (doctor.length === 0) {
+      return res.status(404).json({
+        success: false,
+        error: "No doctors found with the specified specialization",
+      });
+    }
+    res.status(201).json({
+      success: true,
+      doctor,
+    });
+  } catch (error) {
+    res.status(401).json({
       success: false,
       error: error.message,
     });
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { createUser, loginUser } from "../controllers/createUser.js";
 import createDoctor from "../controllers/doctor.js";
-import { getAllDoctors } from "../controllers/getDoctor.js";
+import { getAllDoctors, searchDoctor } from "../controllers/getDoctor.js";
 import { requireAuth } from "../middleware/requireAuth.js";
 const router = express.Router();
 
@@ -12,4 +12,5 @@ router.post("/createDoc", createDoctor);
 //Get All Doctors
 // router.use(requireAuth);
 router.get("/doctors", requireAuth, getAllDoctors);
+router.get("/searchDoctor", requireAuth, searchDoctor);
 export default router;
